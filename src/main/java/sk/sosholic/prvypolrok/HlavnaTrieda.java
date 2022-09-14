7769695dfa5b24fab1e714b22e4c11edc1b7a58d
LubosSukup <Lubos.Sukup@kyndryl.com>
2022-09-14 11:14:02 +0200

    test

---
+++ src/main/java/sk/sosholic/prvypolrok/HlavnaTrieda.java
@@ -0,0 +1,8 @@
+package sk.sosholic.prvypolrok;
+
+public class HlavnaTrieda {
+    public static void main(String[] args) {
+        System.out.println("test");
+
+    }
+}
---
+++ src/main/java/sk/sosholic/prvypolrok/Main.java
@@ -0,0 +1,8 @@
+package sk.sosholic.prvypolrok;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Moj prvy skompilovany program :) ");
+    }
+}
