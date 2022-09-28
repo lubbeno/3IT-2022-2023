206d437a0a79b8bdda9712895fc517ec4577d700
LubosSukup <Lubos.Sukup@kyndryl.com>
2022-09-28 09:07:59 +0200

    kalkulacka

--- src/main/java/sk/sosholic/prvypolrok/kalkulacka/Kalkulacka.java
+++ src/main/java/sk/sosholic/prvypolrok/kalkulacka/Kalkulacka.java
@@ -10,8 +10,7 @@ public class Kalkulacka {
         System.out.println("tento program scitava dve zadane cisla");
         int exit = 1;
        while (exit!=0){
-            System.out.println("Ak chces scitat slac 1 ");System.out.println("Ak chces odcitat slac 2 ");
-            System.out.println("Ak chces nasobit slac 3 ");System.out.println("Ak chces delit slac 4 ");
+            udovnyPopis();
            System.out.print("Zadana matematicka operacia: ");
             int matematickaOperacia=scanner.nextInt();
             switch (matematickaOperacia){
@@ -22,29 +21,42 @@ public class Kalkulacka {
                     int scitanec2 = scanner.nextInt();
                     System.out.println("vysledok scitania je: "+ (scitanec1 + scitanec2));
                     /// DOKONCIT ostatne pripady pre 2 3 4
+                    break;
                 case 2:
                     System.out.print("zadaj prve cislo a stlac enter: ");
                     int odcitanec1 = scanner.nextInt();
                     System.out.print("zadaj druhe cislo a stlac enter: ");
                     int odcitanec2 = scanner.nextInt();
                     System.out.println("vysledok odcitania je: "+ (odcitanec1 - odcitanec2));
+                    break;
                 case 3:
                     System.out.print("zadaj prve cislo a stlac enter: ");
                     int nasobitel1 = scanner.nextInt();
                     System.out.print("zadaj druhe cislo a stlac enter: ");
                     int nasobitel2 = scanner.nextInt();
                     System.out.println("vysledok nasobenia je: "+ (nasobitel2 * nasobitel1));
-
+                    break;
                 case 4:
                     System.out.print("zadaj prve cislo a stlac enter: ");
-                    int delenec = scanner.nextInt();
+                    double delenec = scanner.nextInt();
                     System.out.print("zadaj druhe cislo a stlac enter: ");
-                    int delitel = scanner.nextInt();
-                    System.out.println("vysledok delenia je: "+ (delenec / delitel));
-
+                    double delitel = scanner.nextInt();
+                    if(delitel == 0 ){
+                        System.out.println("memozes delit nulou ");
+                    }else {
+                        System.out.println("vysledok delenia je: " + (delenec / delitel));
+                    }
+                    break;
+                default:
+                    System.out.println("!!!!! Zly vyber !!!!! Zadaj znova");
+                    continue;
             }
             System.out.println("ukoncenie programu stlac 0 a enter. Pre pokracovanie stlac 1");
             exit = scanner.nextInt();
         }
+    }
+    private static void udovnyPopis() {
+        System.out.println("Ak chces scitat slac 1 ");System.out.println("Ak chces odcitat slac 2 ");
+        System.out.println("Ak chces nasobit slac 3 ");System.out.println("Ak chces delit slac 4 ");
     }
 }
